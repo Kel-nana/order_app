301d7607bd9a9f17e80e4647a1bf627bf19703ef
kelvinirungu <kelvinn.irungu69@gmail.com>
2024-02-18 21:14:13 +0300

    fix linter errors

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,18 @@
-import React, { Fragment } from "react";
-import HeaderCartBtn from "./HeaderCartBtn";
+import React, { Fragment } from 'react';
+import HeaderCartBtn from './HeaderCartBtn';
 import mealsImg from '../../assets/mealsE.jpg';
 import classes from './header.module.css';
 
-const Header = props => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>Meals</h1>
-            <HeaderCartBtn />
-        </header>
-        <div className={classes['main_image']}>
-            <img className={classes['main_img']} src={mealsImg} alt="Food Art"/> 
-        </div>
-    </Fragment>
-};
+const Header = () => (
+  <>
+    <header className={classes.header}>
+      <h1>Meals</h1>
+      <HeaderCartBtn />
+    </header>
+    <div className={classes.main_image}>
+      <img className={classes.main_img} src={mealsImg} alt="Food Art" />
+    </div>
+  </>
+);
 
 export default Header;
