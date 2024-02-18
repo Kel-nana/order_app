80f859e62fbcbc58eb9d2c72f0caba24bf813f62
kelvinirungu <kelvinn.irungu69@gmail.com>
2024-02-18 21:15:15 +0300

    fix linter errors

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -1,12 +1,12 @@
-import { Fragment } from "react";
-import MealsSummary from "./MealsSummary";
-import AvailableMeals from "./AvailableMeals";
+import { Fragment } from 'react';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
 
-const Meals = ()=> {
-return <Fragment>
+const Meals = () => (
+  <>
     <MealsSummary />
     <AvailableMeals />
-</Fragment>
-}
+  </>
+);
 
 export default Meals;
