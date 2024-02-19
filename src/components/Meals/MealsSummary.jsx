b24d0ba9a2fdf7098704243a8ceb98480de342e1
kelvinirungu <kelvinn.irungu69@gmail.com>
2024-02-19 10:11:20 +0300

    fixing max length MealSummary

--- src/components/Meals/MealsSummary.jsx
+++ src/components/Meals/MealsSummary.jsx
@@ -1,14 +1,19 @@
-/* eslint-disable max-len */
 import classes from './mealsSummary.module.css';
+
+const summaryData = {
+  header: 'Delicious Food, Delivered to you',
+  introOne: 'Choose your favourite meal from our broad selection of available meals and enjoy a delicious lunch of dinner at home.',
+  introTwo: 'All our meals are cooked with high quality ingredients, just-in-time and of course by experienced chefs',
+};
 
 const MealsSummary = () => (
   <section className={classes.summary}>
-    <h2>Delicious Food, Delivered to you</h2>
+    <h2>{summaryData.header}</h2>
     <p>
-      Choose your favourite meal from our broad selection of available meals and enjoy a delicious lunch of dinner at home.
+      {summaryData.introOne}
     </p>
     <p>
-      All our meals are cooked with high quality ingredients, just-in-time and of course by experienced chefs
+      {summaryData.introTwo}
     </p>
   </section>
 );
